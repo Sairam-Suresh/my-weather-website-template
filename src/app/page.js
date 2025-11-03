e2c96752dab81a14bd939e8b0393ba5dc6a8ba62
Sairam Suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2025-11-03 16:06:59 +0800

    Add maximum width

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-100 p-6 font-sans">
-      <div className="text-2xl font-bold">Welcome!</div>
-      <div>If you see this, you have successfully started running the template application!</div>
-      <div>However, there is definitely more work to be done to turn this into a real weather app. </div>
+      <div className="flex flex-col items-center justify-center max-w-2xl text-center">
+        <div className="text-2xl font-bold">Welcome!</div>
+        <div>If you see this, you have successfully started running the template application!</div>
+        <div>However, there is definitely more work to be done to turn this into a real weather app. </div>
 
-      <div>Now that you can see this, you should have a look at the Readme.md files in src/lib and src/components</div>
+        <div>Now that you can see this, you should have a look at the Readme.md files in src/lib and src/components</div>
+      </div>
     </div>
   );
 }
